51eb21a37ee582afcf85fb90eb71f793092b06e3
obahblack <obahsomto@gmail.com>
2023-06-01 18:44:48 +0100

    final commit

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - Checks f a given node is a root
+ * @node: the pointer to the node to be checked
+ * Return: 1 if node is a root otherwise 0 
+*/
+int binary_tree_is_root(const binary_tree_t *node)
+{
+    if (node == NULL)
+    {
+        return 0;
+    }
+    /* A root node has no parent */
+    if (node->parent == NULL && node != NULL)
+    {
+        return 1;
+    }
+
+    return  0;
+}
