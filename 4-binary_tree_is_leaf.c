5ad030b711c38106fc8757336e83711f742df60d
Praise Obah <obahsomto@gmail.com>
2024-01-05 06:59:53 -0800

    A leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,15 +7,15 @@
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node == NULL)
-    {
-        return 0;
-    }
+	if (node == NULL)
+	{
+		return (0);
+	}
 
-    /* A leaf node has no left or right child */
-    if (node->left == NULL && node->right == NULL)
-    {
-        return 1;
-    }
-    return 0;
+	/* A leaf node has no left or right child */
+	if (node->left == NULL && node->right == NULL)
+	{
+		return (1);
+	}
+	return (0);
 }
