6580336519725bc4c685b2b63eb94cafa001f46a
Praise Obah <obahsomto@gmail.com>
2024-01-05 06:40:28 -0800

    delete

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -28,7 +28,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		temp = parent->left;
 		parent->left = new_node;
 		new_node->left = temp;
-		temp->parent = new_node
+		temp->parent = new_node;
 	}
 	else
 	{
